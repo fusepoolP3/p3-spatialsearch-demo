a432b92753a5f8d9f8fab02e9526ddc40e4a67ce
Luigi Selmi <selmi_luigi@hotmail.com>
2015-01-23 18:10:42 +0100

    initial commit embedded jetty webapp

---
+++ src/main/java/eu/fusepool/p3/osm/demo/Main.java
@@ -0,0 +1,21 @@
+package eu.fusepool.p3.osm.demo;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class Main {
+        
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(Integer.valueOf(8080));
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/demo/");
+        webapp.setResourceBase("src/main/webapp");
+        //webapp.setWelcomeFiles(new String [] {"index.html"});
+        server.setHandler(webapp);
+        server.start();
+        server.join();
+          
+    }
+      
+
+}
